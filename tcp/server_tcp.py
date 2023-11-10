873e6bafd0919022ef07409dbafdf391070f84a9
Susch <94342969+Susch12@users.noreply.github.com>
2023-11-10 04:51:51 +0000

    Update server_tcp.py

--- tcp/server_tcp.py
+++ tcp/server_tcp.py
@@ -19,13 +19,17 @@ def worker(connection, client_address):
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while True:
-    # Wait for other connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    threading.Thread(target=worker, args=(connection, client_address)).start()
 
 # Bind the socket to the port
 server_address = ('192.168.100.134', 8081)
 print('starting up on {} port {}'.format(*server_address>
 sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(5)
+                                         
+while True:
+    # Wait for other connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+    threading.Thread(target=worker, args=(connection, client_address)).start()
